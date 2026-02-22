15ac3cb14b7399ab9743cd2be6927d0e4e50e65e
Mohammed Haitam Yousfi <my9639@g.rit.edu>
2026-02-23 03:42:39 +0400

    Nose and Face Complete

--- Week 3/3.1 Incremental Development/smiley.py
+++ Week 3/3.1 Incremental Development/smiley.py
@@ -1,10 +1,11 @@
 import turtle
 
+'''
 def draw_circle(x,y, radius, fill_color):
-    '''
-    This function draws a filled-in circle with 4 different parameters when called.   
-    We will test this function by defining a main function that uses it to draw 3 circles with different locations, radii, and fill colors. 
-    '''
+'''
+    #This function draws a filled-in circle with 4 different parameters when called.   
+    #We will test this function by defining a main function that uses it to draw 3 circles with different locations, radii, and fill colors. 
+'''
     turtle.up()
     turtle.goto(x,y)
     turtle.down()
@@ -12,8 +13,14 @@ def draw_circle(x,y, radius, fill_color):
     turtle.begin_fill()
     turtle.circle(radius)
     turtle.end_fill()
-    turtle.up()
-    
+    turtle.up()    
+'''
+
+
+def tweak(speed,traceranimation):
+    turtle.speed(speed)
+    turtle.tracer(traceranimation)
+
 
 def draw_centered_circle(x,y, radius, fill_color):
 
@@ -44,8 +51,12 @@ def draw_smiley(x,y, headradius, fill_color_head, fill_color_nose):
 
 
 def main():
+    
+    tweak(0,False)
     draw_smiley(0,0,200,"yellow","pink")
-
+    tweak(10,True)
+    
+    turtle.hideturtle()
     input("Press Enter to Close Program")
 
 main()
