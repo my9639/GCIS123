f8cf580f8a5a302895da9f68b2508350dd5cdaa5
Mohammed Haitam Yousfi <my9639@g.rit.edu>
2026-02-23 16:44:59 +0400

    Constants Practice

--- Week 3/3.2 Debugging Errors/calculator.py
+++ Week 3/3.2 Debugging Errors/calculator.py
@@ -1,3 +1,16 @@
+
+pi = 3.14159
+
+def circumference_given_radius(radius):
+    return 2*pi*radius 
+
+
+
+def area_given_radius(radius):
+    return pi*radius**2
+
+
+
 def addition(number1 , number2):
     return number1 + number2
 
@@ -20,13 +33,18 @@ def division(number1 , number2):
 
 def main():
     
-    number1 = int(input("Number 1: "))
-    number2 = int(input("Number 2: "))
+    # number1 = int(input("Number 1: "))
+    # number2 = int(input("Number 2: "))
     
-    print(addition(number1,number2))
-    print(subtraction(number1,number2))
-    print(multiplication(number1,number2))
-    print(division(number1,number2))
+    radius = float(input("Input a radius to get the circle's circumference & area: "))
+
+    print(circumference_given_radius(radius))
+    print(area_given_radius(radius))
+
+    # print(addition(number1,number2))
+    # print(subtraction(number1,number2))
+    # print(multiplication(number1,number2))
+    # print(division(number1,number2))
 
 
 
