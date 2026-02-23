735969934eac75d883a73bd8c336a5e80cbc72d4
Mohammed Haitam Yousfi <my9639@g.rit.edu>
2026-02-23 04:00:01 +0400

    Nose and Face Complete

--- Week 3/3.1 Incremental Development/smiley.py
+++ Week 3/3.1 Incremental Development/smiley.py
@@ -2,10 +2,10 @@ import turtle
 
 '''
 def draw_circle(x,y, radius, fill_color):
-'''
+
     #This function draws a filled-in circle with 4 different parameters when called.   
     #We will test this function by defining a main function that uses it to draw 3 circles with different locations, radii, and fill colors. 
-'''
+
     turtle.up()
     turtle.goto(x,y)
     turtle.down()
@@ -16,14 +16,18 @@ def draw_circle(x,y, radius, fill_color):
     turtle.up()    
 '''
 
-
 def tweak(speed,traceranimation):
+    '''
+    This function will tweak the speed and animation of the turtle as we like.
+    '''
     turtle.speed(speed)
     turtle.tracer(traceranimation)
 
 
 def draw_centered_circle(x,y, radius, fill_color):
-
+    '''
+    This function always draws a circle with the turtle in the middle of the circle.
+    '''
     original_heading = turtle.heading()
 
     print(int(turtle.heading()))
@@ -44,19 +48,29 @@ def draw_centered_circle(x,y, radius, fill_color):
 
 
 def draw_smiley(x,y, headradius, fill_color_head, fill_color_nose):
-    
+    '''
+    This function draws the Head and Nose of the smiley face.    
+    '''
     draw_centered_circle(x,y, headradius, fill_color_head)
     
     draw_centered_circle (x,y, headradius/10, fill_color_nose)
 
+def draw_eyes(x,y, eyeradius, color):
+    '''
+    This function draws the Eyes of the smiley face.
+    '''
+    draw_centered_circle(x,y, eyeradius, "White") #Draws the Eyeball
+    draw_centered_circle(x,y, eyeradius/2, "Light Blue") #Draws the iris
+    draw_centered_circle(x,y, eyeradius/4, "Black") #Draws the pupil
 
 def main():
     
     tweak(0,False)
-    draw_smiley(0,0,200,"yellow","pink")
+    #draw_smiley(0,0,200,"yellow","pink")
     tweak(10,True)
+    draw_eyes(10,10,100,"brown")
     
-    turtle.hideturtle()
+    #turtle.hideturtle()
     input("Press Enter to Close Program")
 
 main()
