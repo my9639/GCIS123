3cb51965ca593f5a4efab5bbf1f0dc55cb2ac71d
Mohammed Haitam Yousfi <my9639@g.rit.edu>
2026-02-24 07:39:21 +0400

    triangle_test before refactoring

--- Week 3/3.3 TDD/activities_test.py
+++ Week 3/3.3 TDD/activities_test.py
@@ -1,11 +1,21 @@
 def square_area(sidelength):
+    
+    if sidelength < 0:
+        return None
+    
     return sidelength * sidelength
+    
+    
 
 def test_square_area_8():
-    value = square_area(8)
-    assert value == 64
+    assert square_area(8) == 64
+
 
 
 def test_square_area_6():
-    value = square_area(6)
-    assert value == 36
+    assert square_area(6) == 36
+
+
+
+def test_square_area_negativesidelength():
+    assert square_area(-5) == None
---
+++ Week 3/3.3 TDD/triangle_test.py
@@ -0,0 +1,14 @@
+def triangle_area(base,height):
+    if base < 0:
+        return None
+    
+    return 0.5 * base * height
+
+def test_triangle_area_0_0():
+    assert triangle_area(0,0) == 0
+
+def test_triangle_area_3_4():
+    assert triangle_area(3,4) == 6
+
+def test_triangle_area_negativebase():
+    assert triangle_area(-3,4) is None
